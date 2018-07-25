78e036c72e604a214ef821cfe43fca6581d18b39
Zhifan Lan <everydaysiling@gmail.com>
2018-07-24 23:12:00 -0600

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private int point = 0;
+	private float Timer = 0.0f;
+
+	public Text pointText;
+	public Text timerText;
+	public Text statusText;
+	public GameObject panel;
+
+	public Rigidbody rb;
+
+    public Button continueBtn;
+	public Button startBtn;
+
+	void Start ()
+	{
+		
+		rb = GetComponent<Rigidbody>();
+
+
+		panel = GameObject.FindGameObjectsWithTag("panel")[0];
+		panel.gameObject.SetActive (false);
+
+//		Button btn = continueBtn.GetComponent<Button>();
+//		btn.onClick.AddListener(changeTimeScale);
+//
+//		Button sBtn = startBtn.GetComponent<Button>();
+//		sBtn.onClick.AddListener(restartGame);
+
+
+	}
+
+	void Update (){
+		
+		Timer += Time.deltaTime;
+		timerText.text = "TIMER:   " + (int)Timer + "s";
+		pointText.text =  "SCORE:   " + point;
+		if (Input.GetKeyDown (KeyCode.Space) && statusText.text == "") {
+			changeTimeScale ();
+		}
+		if (Input.GetKey("escape"))
+			Application.Quit();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+		if (other.gameObject.CompareTag ("score"))
+		{
+			other.gameObject.SetActive (false);
+			point += 2;
+			pointText.text = "SCORE:   " + point.ToString ();
+			if (point >= 20) {
+				//win
+				statusText.text =  "YOU WIN!";
+				changeTimeScale();
+				continueBtn.interactable = false;
+
+			}
+		}
+		if (other.gameObject.CompareTag ("bomb"))
+		{
+			//lose
+			statusText.text =  "YOU LOSE!";
+			changeTimeScale();
+			continueBtn.interactable = false;
+		}
+	}
+
+
+	public void changeTimeScale () {
+		if (Time.timeScale != 0.0f) {
+			Time.timeScale = 0.0f;
+			panel.gameObject.SetActive(true);
+		} 
+		else {
+			Time.timeScale = 1.0f;
+			panel.gameObject.SetActive(false);
+		}
+	}
+
+	public void restartGame () {
+
+		changeTimeScale();
+		continueBtn.interactable = true;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);	
+		
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Rotator : MonoBehaviour
+{
+	
+	private Vector3 targetPosition, currPosition;
+	private float vary = 2f;
+	private int speed = 5;
+	private Vector3 velocity = Vector3.zero;
+
+	void Start () {
+		targetPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+
+		currPosition = transform.position;
+
+		if (currPosition != targetPosition) {
+			transform.position = Vector3.SmoothDamp (currPosition, targetPosition, ref velocity, speed * Time.deltaTime);
+		} else {
+
+			float angle = Vector3.Angle (currPosition, targetPosition);
+
+			while (true) {
+				
+				float angleDiff = Random.Range (-180.0f, 180.0f);
+
+				angle += angleDiff;
+
+
+				float degree = angle * Mathf.PI / 180.0f;
+
+				targetPosition = new Vector3 (
+					transform.position.x + vary * Mathf.Cos (degree),
+					1, 
+					transform.position.z + vary * Mathf.Sin (degree)
+				);
+
+				// if the target is in the arena, break the loop
+				if (-9 <= targetPosition.x && targetPosition.x <= 9 && -9 <= targetPosition.z && targetPosition.z <= 9) {
+					break;
+				}
+			}
+				
+		}
+	}
+		
+
+
+	}
+		
